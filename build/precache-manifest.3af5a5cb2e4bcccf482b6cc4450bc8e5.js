68389036e7b4c15b3ef6f4ed83775652e0a430f8
Ding <damdyx@163.com>
2022-12-01 19:50:46 +0800

    fix

---
+++ build/precache-manifest.3af5a5cb2e4bcccf482b6cc4450bc8e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ad278b680f2d312a9d5",
+    "url": "/personal-income-tax-calculator/static/css/main.b8da5d2f.chunk.css"
+  },
+  {
+    "revision": "4ad278b680f2d312a9d5",
+    "url": "/personal-income-tax-calculator/static/js/main.4ad278b6.chunk.js"
+  },
+  {
+    "revision": "b993ff48f881b569d630",
+    "url": "/personal-income-tax-calculator/static/js/runtime~main.b993ff48.js"
+  },
+  {
+    "revision": "2e4f16d4a10633709fba",
+    "url": "/personal-income-tax-calculator/static/js/2.2e4f16d4.chunk.js"
+  },
+  {
+    "revision": "e13c3c68680467469d8d",
+    "url": "/personal-income-tax-calculator/static/js/3.e13c3c68.chunk.js"
+  },
+  {
+    "revision": "94a815faaf519d3b369c",
+    "url": "/personal-income-tax-calculator/static/js/4.94a815fa.chunk.js"
+  },
+  {
+    "revision": "f43a6796e6a68324c5f0aaec88e78890",
+    "url": "/personal-income-tax-calculator/index.html"
+  }
+];
